2c51bc6da9d6b91783997482f2e07ac2c2080924
Abb4 <abb.coding@proton.me>
2022-09-25 12:12:59 +0200

    simplified code, added docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::{Vec2, Vec3};
 
+/// Produces a `Vec3` at least `min_distance` and at most `max_distance` away from `origin` taking `origin.z` as its z-component.
 pub fn rand_vec3_xy(origin: Vec3, min_distance: f32, max_distance: f32) -> Vec3 {
     assert!(
         min_distance <= max_distance,
@@ -20,12 +21,10 @@ pub fn rand_vec2(origin: Vec2, min_distance: f32, max_distance: f32) -> Vec2 {
         "Min distance is greater than max distance when generating a vector with offset."
     );
 
-    let vec: Vec<f32> = Vec::from([
-        random_f32_signed(min_distance, max_distance),
-        random_f32_signed(min_distance, max_distance),
-    ]);
-
-    origin + Vec2::from_slice(&vec)
+    Vec2::new(
+        origin.x + random_f32_signed(min_distance, max_distance),
+        origin.y + random_f32_signed(min_distance, max_distance),
+    )
 }
 
 /// Produces a `Vec3` at least `min_distance` and at most `max_distance` away from `origin`.
@@ -35,13 +34,11 @@ pub fn rand_vec3(origin: Vec3, min_distance: f32, max_distance: f32) -> Vec3 {
         "Min distance is greater than max distance when generating a vector with offset."
     );
 
-    let vec: Vec<f32> = Vec::from([
-        random_f32_signed(min_distance, max_distance),
-        random_f32_signed(min_distance, max_distance),
-        random_f32_signed(min_distance, max_distance),
-    ]);
-
-    origin + Vec3::from_slice(&vec)
+    Vec3::new(
+        origin.x + random_f32_signed(min_distance, max_distance),
+        origin.y + random_f32_signed(min_distance, max_distance),
+        origin.z + random_f32_signed(min_distance, max_distance),
+    )
 }
 
 /// Produces a `f32` between `lower_bound` and `upper_bound`.
